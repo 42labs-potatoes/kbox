bd735058a167099d1809f8755babbc64baea65cc
James Cheuk <jccf091@gmail.com>
2014-10-20 11:51:09 +0800

    reset config puma

--- config/puma.rb
+++ config/puma.rb
@@ -1,18 +1,15 @@
-workers Integer(ENV['PUMA_WORKERS'] || 3)
-threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 16)
+threads (ENV['PUMA_MIN_THREADS']|| 0),(ENV['PUMA_MAX_THREADS']|| 16)
+workers (ENV['PUMA_WORKERS'] || 2)
 
 preload_app!
 
-rackup      DefaultRackup
-port        ENV['PORT']     || 3000
-environment ENV['RACK_ENV'] || 'development'
-
 on_worker_boot do
-  # worker specific setup
+  ActiveRecord::Base.connection_pool.disconnect!
+
   ActiveSupport.on_load(:active_record) do
-    config = ActiveRecord::Base.configurations[Rails.env] ||
-        Rails.application.config.database_configuration[Rails.env]
-    config['pool'] = ENV['MAX_THREADS'] || 16
+    config = ActiveRecord::Base.configurations[Rails.env] || Rails.application.config.database_configuration[Rails.env]
+    config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
+    config['pool']              = ENV['DB_POOL'] || 5
     ActiveRecord::Base.establish_connection(config)
   end
 end
